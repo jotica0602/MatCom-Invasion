c6cc4a965c2212179dc27605488897ab1f054882
jotica0602 <alejandroeb5271@gmail.com>
2024-08-10 00:06:45 -0400

    fixing console dimensions

--- globals.c
+++ globals.c
@@ -6,15 +6,15 @@ pthread_mutex_t grid_lock;
 int g_max;
 int g_is_over = false;
 int g_score = 0;
-int g_lives = 5;
+int g_lives = 3;
 int g_win_flag = false;
 int g_current_level = 1;
 
 void reset_globals(){
-    g_max = __INT32_MAX__;
+    g_max = -__INT32_MAX__;
     g_is_over = false;
     g_score = 0;
-    g_lives = 5;
+    g_lives = 3;
     g_win_flag = false;
 }
 
