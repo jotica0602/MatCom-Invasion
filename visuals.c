3c2045f188fed205f0aea1cae371db450c90589a
jotica0602 <alejandroeb5271@gmail.com>
2024-08-20 18:31:51 -0400

    welcome screen resized

--- visuals.c
+++ visuals.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 
 void welcome(){
-    printf("\n\t\t\t\t\t\t\t\tWELCOME SOLDIER! \n \n");
+    printf("\n\t\t\t\tWELCOME SOLDIER! \n \n");
     sleep(2);
-    printf("\t\t\t\t\tTHE COMMAND PROMPT INVADERS ARE BACK AND EVERYONE NEEDS A HERO \n \n ");
+    printf("\tTHE COMMAND PROMPT INVADERS ARE BACK AND EVERYONE NEEDS A HERO \n \n ");
     sleep(2);
-    printf("\t\t\t\t\t\t  DEFEND YOUR OPERATING SYSTEM NO MATTER WHAT\n \n");
+    printf("\t\tDEFEND YOUR OPERATING SYSTEM NO MATTER WHAT\n \n");
     sleep(2);
-    printf("\t\t\t\t\t\t\t\tGOOD LUCK!!!");
+    printf("\t\t\t\tGOOD LUCK!!!");
     sleep(2);
-    printf("\n\n\t\t\t\t\t\t\t   PRESS ANY KEY TO START");
+    printf("\n\n\t\t\t  PRESS ANY KEY TO START");
     getchar();
 }
 
