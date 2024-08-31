1101c12402840836bffa66547b1f366d568fbd8e
jotica0602 <alejandroeb5271@gmail.com>
2024-08-31 16:58:20 -0400

    run game script

---
+++ run.sh
@@ -0,0 +1,3 @@
+gcc -o MatComInvasion bullet.c enemy.c globals.c main.c player.c terminal.c visuals.c
+sudo cp MatComInvasion /usr/local/bin/MatComInvasion
+sudo chmod +x /usr/local/bin/MatComInvasion
