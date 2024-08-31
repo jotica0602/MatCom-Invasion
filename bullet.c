859863d18bec59a8447446a9ff5dfcf2396fd60b
jotica0602 <alejandroeb5271@gmail.com>
2024-08-31 16:55:44 -0400

    refactor

--- bullet.c
+++ bullet.c
@@ -1,79 +1,55 @@
 #include "bullet.h"
+#include "globals.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
-Bullet * new_player_bullet(int *pPlayerx, int *pPlayery){
-    Bullet *player_bullet = (Bullet*)malloc(sizeof(Bullet));
-    player_bullet->x = *pPlayerx - 1;
-    player_bullet->y = *pPlayery;
-    return player_bullet;
+void new_enemy_bullet(Bullet *bullet, int enemy_index){
+    bullet->is_active = true;
+    bullet->x = enemies[enemy_index].x + 1;
+    bullet->y = enemies[enemy_index].y;
+    grid[bullet->x + 1][bullet->y] = grid[bullet->x + 1][bullet->y] == 'M' ? 'M' : '*';
+    enemy_bullet_index = (enemy_bullet_index + 1) % NUM_ENEMIES;
+    active_enemy_bullets++;
 }
 
-Bullet * new_enemy_bullet(int *pEnemyx, int *pEnemyy){
-    Bullet *player_bullet = (Bullet*)malloc(sizeof(Bullet));
-    player_bullet->x = *pEnemyx + 1;
-    player_bullet->y = *pEnemyy;
-    return player_bullet;
-}
-
-void free_enemy_bullets(Bullet *bullets[]){
-    for(int i = 0; i < NUM_ENEMIES; i++){
-        free(bullets[i]);
-    }
-}
-
-void generate_enemy_bullet(Enemy * enemies[], char **grid, int *pRows, int *pCols, Bullet *bullets[], int *bullet_index, int *bullets_count){
-    pthread_mutex_lock(&grid_lock);
-    if(*bullets_count == NUM_ENEMIES) {
-        pthread_mutex_unlock(&grid_lock);
-        return;} // only NUM_ENEMIES bullets are allowed
-    int random;                     // declaring random number
+void generate_enemy_bullet(){
+    if(active_enemy_bullets == NUM_ENEMIES){ // only NUM_ENEMIES bullets are allowed
+        return;
+    } 
     for(int i = NUM_ENEMIES - 1; i >= 0; i--){
-        if(enemies[i]->is_alive && (rand()%607 == 0)){ // only alive enemies can shoot
-            grid[enemies[i]->x+1][enemies[i]->y] = grid[enemies[i]->x+1][enemies[i]->y] == 'M' ? 'M' : '*';
-            bullets[*bullet_index] = new_enemy_bullet(&enemies[i]->x,&enemies[i]->y);
-            *bullet_index = (*bullet_index + 1)%NUM_ENEMIES;
-            *bullets_count++;
-            // printf("%d", *bullets_count);
+        if(enemies[i].is_alive && (rand() % 607 == 0)){ // only alive enemies can shoot
+            new_enemy_bullet(&enemy_bullets[enemy_bullet_index], i);
             break;
         }
     }
-    pthread_mutex_unlock(&grid_lock);
 }
 
-void move_enemy_bullets(char **grid, Bullet *bullets[], int *pRows, int *bullets_count){
+void move_enemy_bullets(){
     for(int i = 0; i < NUM_ENEMIES; i++){
-        if(bullets[i] != NULL){
-            if(bullets[i]->x == *pRows - 1){
-                grid[bullets[i]->x][bullets[i]->y] = ' ';
-                bullets[i] = NULL;
-                *bullets_count--;
+        if(enemy_bullets[i].is_active && enemy_bullets[i].x == ROWS - 1){
+            grid[enemy_bullets[i].x][enemy_bullets[i].y] = ' ';
+            enemy_bullets[i].is_active = false;
+            active_enemy_bullets--;
+            continue;
+        } 
+        else if(enemy_bullets[i].is_active && enemy_bullets[i].x < ROWS - 1) {
+            if(grid[enemy_bullets[i].x][enemy_bullets[i].y] == 'M'){
+                enemy_bullets[i].x++;
                 continue;
-            }else{
-                grid[bullets[i]->x][bullets[i]->y] = grid[bullets[i]->x][bullets[i]->y] == 'M' ? 'M' : ' ';
-                bullets[i]->x++;
-                grid[bullets[i]->x][bullets[i]->y] = '*';
-
+            } else {
+                grid[enemy_bullets[i].x][enemy_bullets[i].y] = grid[enemy_bullets[i].x][enemy_bullets[i].y] == 'M' ? 'M' : ' ';
+                enemy_bullets[i].x++;
+                grid[enemy_bullets[i].x][enemy_bullets[i].y] = '*';
             }
         }
     }
-}
+}   
 
-EnemyBulletThreadParams *new_enemy_bullet_thread_params(char **grid, Bullet *bullets[], int *pRows, int *bullets_count, int *terminate){
-    EnemyBulletThreadParams *ebtp = (EnemyBulletThreadParams*)malloc(sizeof(EnemyBulletThreadParams));
-    ebtp->grid = grid;
-    ebtp->bullets = bullets;
-    ebtp->pRows = pRows;
-    ebtp->bullets_count = bullets_count;
-    ebtp->terminate = terminate;
-    return ebtp;
-}
-
-void *enemy_bullet_thread_function(void *params){
-    EnemyBulletThreadParams *ebtp = (EnemyBulletThreadParams*)params;
-    while(!(*ebtp->terminate)){
+void *enemy_bullet_routine(void *params){
+    while(!terminate){
         pthread_mutex_lock(&grid_lock);
-        move_enemy_bullets(ebtp->grid, ebtp->bullets, ebtp->pRows, ebtp->bullets_count);
+        move_enemy_bullets();
         pthread_mutex_unlock(&grid_lock);
         usleep(100000);  // wait 0.01 secs before moving again
     }
@@ -81,75 +57,46 @@ void *enemy_bullet_thread_function(void *params){
     return NULL;
 }
 
-// Player bullet thread parameters
-PlayerBulletThreadParams *new_player_bullet_thread_params(char **grid, Bullet **player_bullet, int *terminate, Player *player){
-    PlayerBulletThreadParams *pbp = (PlayerBulletThreadParams*)malloc(sizeof(PlayerBulletThreadParams));
-    pbp->grid = grid;
-    pbp->player_bullet = player_bullet;
-    pbp->terminate = terminate;
-    pbp->player = player;
-    return pbp;
-}
-
-// When you pass a pointer to a function, you are creating a copy of that pointer, so we need to make a pointer to that pointer. 
-// This way we can modify the original data.
-void move_player_bullet(Bullet **player_bullet, char **grid, Player *player) {
-    // pthread_mutex_lock(&grid_lock);
-    
-    if (*player_bullet != NULL && (*player_bullet)->x == 1) {
-        grid[(*player_bullet)->x][(*player_bullet)->y] = ' ';
-        free(*player_bullet);
-        *player_bullet = NULL;  // Esto ahora modificará el puntero original
-        player->can_shoot = false;
+void move_player_bullet(){
+    if (player_bullet.is_active && player_bullet.x == 1) {
+        grid[player_bullet.x][player_bullet.y] = ' ';
+        player_bullet.is_active = false;
+        player.can_shoot = false;
     }
-    else if (*player_bullet != NULL && (*player_bullet)->x != 0) {
-        grid[(*player_bullet)->x][(*player_bullet)->y] = ' ';
-        (*player_bullet)->x--;
-        grid[(*player_bullet)->x][(*player_bullet)->y] = '^';
+    else if (player_bullet.is_active && player_bullet.x > 1) {
+        grid[player_bullet.x][player_bullet.y] = ' ';
+        player_bullet.x--;
+        grid[player_bullet.x][player_bullet.y] = '^';
     }   
-    else{return;}
-
-    // pthread_mutex_unlock(&grid_lock);
 }
 
-void *player_bullet_movement_thread_function(void *params){
-    PlayerBulletThreadParams *pbp = (PlayerBulletThreadParams*)params;
-    while(!(*pbp->terminate)){
+void *player_bullet_movement_routine(void *params){
+    while(!terminate){
         pthread_mutex_lock(&grid_lock);
-        move_player_bullet(pbp->player_bullet, pbp->grid, pbp->player);
+        move_player_bullet();
         pthread_mutex_unlock(&grid_lock);
         usleep(50000);
     }
 }
 
-int check_player_bullet_collision(char ** grid, int * pRows, int * pCols, Enemy * enemies[]){
-    pthread_mutex_lock(&grid_lock);
+int check_player_bullet_collision(){
+    
     for(int i = 0; i < NUM_ENEMIES; i++){
-        if(grid[enemies[i]->x + 1][enemies[i]->y] == '^' && enemies[i]->is_alive){
-            enemies[i]->is_alive = false;
-            grid[enemies[i]->x][enemies[i]->y] = 'X';
+        if(player_bullet.is_active && player_bullet.x == enemies[i].x && player_bullet.y == enemies[i].y && enemies[i].is_alive){
+            enemies[i].is_alive = false;
+            grid[enemies[i].x][enemies[i].y] = 'X';
             g_score++;
-            pthread_mutex_unlock(&grid_lock);
             return true;
         }
     }
-    pthread_mutex_unlock(&grid_lock);
     return false;
 }
 
-
-int check_enemy_bullet_collision(char **grid, Player *player){
-    pthread_mutex_lock(&grid_lock);
+int check_enemy_bullet_collision(){
     int collision = false;
-    if(grid[player->x-1][player->y] == '*'){
-        grid[player->x-1][player->y] = ' ';
+    if(grid[player.x-1][player.y] == '*'){
+        grid[player.x-1][player.y] = ' ';
         collision = true;
     }
-    if(grid[player->x][player->y] == '*'){
-        grid[player->x][player->y] = ' ';
-        g_lives--;
-        collision = true;
-    }
-    pthread_mutex_unlock(&grid_lock);
     return collision;
 }
--- bullet.h
+++ bullet.h
@@ -2,65 +2,31 @@
 #define BULLET_H
 #include <stdlib.h>
 #include <stdio.h>
-#include "enemy.h"
-#include "globals.h"
-#include "player.h"
 
-typedef struct Bullet{
+typedef struct{
     int x;
     int y;
-}Bullet;
-
-typedef struct EnemyBulletThreadParams{
-    char **grid;
-    Bullet ** bullets;
-    int *pRows;
-    int *bullets_count;
-    int *terminate;
-}EnemyBulletThreadParams;
-
-typedef struct PlayerBulletThreadParams{
-    char **grid;
-    Bullet **player_bullet;
-    int *pRows;
-    int *terminate;
-    Player *player;
-}PlayerBulletThreadParams;
-
-// Player bullet constructor.
-Bullet *new_player_bullet(int *pPlayerx, int *pPlayery);
-
-// Enemy bullet constructor.
-Bullet *new_enemy_bullet(int *pEnemyx, int *pEnemyy);
-
-// Enemy bullets destructor
-void free_enemy_bullets(Bullet *bullets[]);
+    int is_active;
+} Bullet;
 
 // Enemy bullet generator.
-void generate_enemy_bullet(Enemy * enemies[], char **grid, int *pRows, int *pCols, Bullet *bullets[], int *bullet_index, int *bullets_count);
+void generate_enemy_bullet();
 
 // Enemy bullet movement handler.
-void move_enemy_bullets(char **grid, Bullet *bullets[], int *pRows, int *bullets_count);
-
-// Enemy bullet thread parameters.
-EnemyBulletThreadParams *new_enemy_bullet_thread_params(char **grid, Bullet *bullets[], int *pRows, int *bullets_count, int *terminate);
+void move_enemy_bullets();
 
 // Enemy bullet movement thread.
-void *enemy_bullet_thread_function(void *params);
+void *enemy_bullet_routine(void *params);
 
 // Player bullet movement handler
-void move_player_bullet(Bullet **player_bullet, char **grid, Player *player);
-
-// Player bullet thread parameters
-PlayerBulletThreadParams *new_player_bullet_thread_params(char **grid, Bullet **player_bullet, int *terminate, Player *player);
+void move_player_bullet();
 
 // Player bullet movement thread
-void *player_bullet_movement_thread_function(void *params);
+void *player_bullet_movement_routine(void *params);
 
 // Player bullet collision handler.
-int check_player_bullet_collision(char ** grid, int * pRows, int *pCols, Enemy * enemies[]);
-
+int check_player_bullet_collision();
 
 // Checks for enemy bullets collision. If there was any, player will lose 1 live.
-int check_enemy_bullet_collision(char **grid, Player *player);
+int check_enemy_bullet_collision();
 #endif
