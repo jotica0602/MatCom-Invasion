29638d56b5eb38bc277f62924d561c9cd9baa24b
jotica0602 <alejandroeb5271@gmail.com>
2024-08-31 16:54:02 -0400

    enemy movement boundaries handler fixed

--- enemy.c
+++ enemy.c
@@ -1,92 +1,41 @@
 #include "enemy.h"
+#include "globals.h"
+#include <stdbool.h>
+#include <unistd.h>
 
-Enemy *new_enemy(int row, int col){
-    Enemy *enemy = (Enemy *)malloc(sizeof(Enemy));
-    enemy->x = row;
-    enemy->y = col;
-    enemy->is_alive = true;
-    return enemy;
-}
-
-void free_enemies(Enemy * enemies[]){
+void handle_enemies_movement() {
+    int boundary_flag = false;
     for(int i = 0; i < NUM_ENEMIES; i++){
-        free(enemies[i]);
-    }
-}
-
-EnemiesMovementParams * new_Enemy_Movement_Params(Enemy * enemies[], int *pRows, int *pCols, char **grid, int *direction, int * terminate){
-    EnemiesMovementParams *aemp = (EnemiesMovementParams*)malloc(sizeof(EnemiesMovementParams));
-    aemp->direction = direction;
-    aemp->enemies = enemies;
-    aemp->grid = grid;
-    aemp->num_enemies = NUM_ENEMIES;
-    aemp->pCols = pCols;
-    aemp->pRows = pRows;
-    aemp->terminate = terminate;
-    return aemp;
-}
-
-void handle_enemy_movement(Enemy *enemy, int *pRows, int *pCols, char ** grid, int *direction, enum Collision * collision) {
-    // enemy->is_alive = false;
-    if(!enemy->is_alive) return;    
-    g_max = g_max < enemy->x ? enemy->x : g_max;
-    grid[enemy->x][enemy->y] = ' ';
-    
-    // Check for boundary conditions
-    if (enemy->y == 1 || enemy->y == *pCols -2 && enemy->is_alive){
-        *collision = enemy->y == 1 ? LEFT_COLLISION : RIGHT_COLLISION;
-        return;
-    }
-
-    enemy->y = *direction == LEFT ? enemy->y - 1 : enemy->y + 1;
-    // Place the enemy in the new position
-    grid[enemy->x][enemy->y] = enemy->is_alive ? 'M' : ' ';   
-}
-
-void handle_enemies_movement(Enemy * enemies[], int num_enemies, int *pRows, int *pCols, char **grid, int * direction) {
-    enum Collision collision = NO_COLLISION;
-    if(*direction == RIGHT){
-        // loop backwards
-        for (int i = NUM_ENEMIES - 1; i >= 0; i--){
-            handle_enemy_movement(enemies[i], pRows, pCols, grid, direction, &collision);
-            if(collision != NO_COLLISION){
-                handle_enemy_collision(enemies, num_enemies, pRows, pCols, grid, direction, &collision);
-                break;
-            }
+        if(!enemies[i].is_alive) continue;
+        grid[enemies[i].x][enemies[i].y] = ' ';
+        enemies[i].y = direction == LEFT ? enemies[i].y - 1 : enemies[i].y + 1;
+        
+        if(enemies[i].y == 1 || enemies[i].y == COLS - 2){
+            boundary_flag = true;
         }
+        grid[enemies[i].x][enemies[i].y] = 'M'; 
     }
 
-    else{
-        // loop forward 
-        for (int i = 0; i < NUM_ENEMIES; i++){
-            handle_enemy_movement(enemies[i], pRows, pCols, grid, direction, &collision);
-            if(collision != NO_COLLISION){
-                handle_enemy_collision(enemies, num_enemies, pRows, pCols, grid, direction, &collision);
-                break;
-            }
+    if(boundary_flag){
+        direction = !direction;
+        for(int i = 0; i < NUM_ENEMIES; i++){
+            if(!enemies[i].is_alive) continue;
+            grid[enemies[i].x][enemies[i].y] = ' ';
+            enemies[i].x++;
+            enemies[i].y = direction == LEFT ? enemies[i].y - 1 : enemies[i].y + 1;
+            grid[enemies[i].x][enemies[i].y] = 'M'; 
+            g_max = g_max < enemies[i].x ? enemies[i].x : g_max;
         }
     }
 }
 
-void handle_enemy_collision(Enemy * enemies[],int num_enemies, int *pRows, int *pCols, char **grid, int * direction, enum Collision * collision){
-    for(int i = 0; i < NUM_ENEMIES; i++){
-        if(!enemies[i]->is_alive) continue;
-        grid[enemies[i]->x][enemies[i]->y] = ' ';
-        enemies[i]->x++;
-        enemies[i]->y = *collision == LEFT_COLLISION ? enemies[i]->y + 1 : enemies[i]->y - 1;
-        grid[enemies[i]->x][enemies[i]->y] = 'M';
-        g_max = enemies[i]->is_alive && g_max < enemies[i]->x ? enemies[i]->x : g_max;
-    }
-    *direction = (*direction + 1) % 2;
-    *collision = NO_COLLISION;
-}
-
-void *enemies_movement_thread(void *params){
-    EnemiesMovementParams *aemp = (EnemiesMovementParams*)params;
-    while(!(*aemp->terminate)){
+void *enemies_movement_routine(void *params){
+    while(!terminate){
         pthread_mutex_lock(&grid_lock);
-        handle_enemies_movement(aemp->enemies, aemp->num_enemies, aemp->pRows, aemp->pCols, aemp->grid, aemp->direction);
+        
+        handle_enemies_movement();
         pthread_mutex_unlock(&grid_lock);
+        // usleep(10000);
         usleep(500000);
     }
     return NULL;
--- enemy.h
+++ enemy.h
@@ -1,27 +1,11 @@
 #ifndef ENEMY_H
 #define ENEMY_H
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
-#include "globals.h"
 
-typedef struct Enemy{
+typedef struct{
     int y;
     int x;
     int is_alive;
 } Enemy;
-
-typedef struct EnemiesMovementParams{
-    Enemy **enemies;
-    int num_enemies;
-    int *pRows;
-    int *pCols;
-    char **grid;
-    int *direction;
-    int *terminate; // termination flag
-} EnemiesMovementParams;
 
 enum Collision{
     LEFT_COLLISION = 0,
@@ -29,27 +13,11 @@ enum Collision{
     NO_COLLISION = 2,
 };
 
-// Enemy constructor.
-Enemy *new_enemy(int row, int col);
-
-// Enemy destructor.
-void free_enemies(Enemy * enemies[]);
-
-// Enemy movement thread params.
-EnemiesMovementParams * new_Enemy_Movement_Params(struct Enemy * enemies[], int *pRows, int *pCols, char **grid, int *direction, int * terminate);
-
-// Single Enemy movement handling.
-void handle_enemy_movement(Enemy *enemy, int *pRows, int *pCols, char ** grid, int *direction, enum Collision * collision);
-
 // Multiple Enemies movement handling.
-void handle_enemies_movement(Enemy * enemies[], int num_enemies, int *pRows, int *pCols, char **grid, int * direction);
+void handle_enemies_movement();
 
-// Enemy left and right collision handler.
-void handle_enemy_collision(Enemy * enemies[],int num_enemies, int *pRows, int *pCols, char **grid, int * direction, enum Collision * collision);
-
-// Enemies movement handler thread.
-void *enemies_movement_thread(void *params);
-
+// Enemies movement handler routine
+void *enemies_movement_routine(void *params);
 
 
 #endif
