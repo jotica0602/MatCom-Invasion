81803c19a6f118bb086351f4f524a84a48d490fe
jotica0602 <alejandroeb5271@gmail.com>
2024-09-01 21:12:01 -0400

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gcc -o MatComInvasion bullet.c enemy.c globals.c main.c player.c terminal.c visuals.c
+gcc -o MatComInvasion bullet.c enemy.c globals.c main.c player.c terminal.c visuals.c shield.c mothership.c
 sudo cp -f MatComInvasion /usr/local/bin/MatComInvasion
 rm -rf MatComInvasion
 sudo chmod +x /usr/local/bin/MatComInvasion
