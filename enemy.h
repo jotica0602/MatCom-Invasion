0363c34a943f84affdd0116521a5e0c62baae944
jotica0602 <alejandroeb5271@gmail.com>
2024-09-01 21:17:39 -0400

    update

--- enemy.h
+++ enemy.h
@@ -8,15 +8,6 @@ typedef struct{
     char type;
 } Enemy;
 
-enum Collision{
-    LEFT_COLLISION = 0,
-    RIGHT_COLLISION = 1,
-    NO_COLLISION = 2,
-};
-
-
 void handle_enemies_movement();                 // Multiple Enemies movement handling.
 void *enemies_movement_routine(void *params);   // Enemies movement handler routine
-
-
 #endif
