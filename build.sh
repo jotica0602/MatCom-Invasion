49e4a4ef5bad55e5086cee37ac2fa5f9556859de
jotica0602 <alejandroeb5271@gmail.com>
2024-09-04 17:03:14 -0400

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 cd Game
 gcc -o MatComInvasion bullet.c enemy.c globals.c main.c player.c terminal.c visuals.c shield.c mothership.c sounds.c
 sudo cp -f MatComInvasion /usr/local/bin/MatComInvasion
+sudo rm -rf MatComInvasion
 cd ..
 sudo cp -r -f Sounds /usr/local/bin/
 sudo touch -f /usr/local/bin/highscore.txt
-rm -rf MatComInvasion
 sudo chmod +x /usr/local/bin/MatComInvasion
 sudo chmod u+s /usr/local/bin/MatComInvasion
